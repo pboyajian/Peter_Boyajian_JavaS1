3f6cd7d18ba76b84b82cb432b6da9d2fa43182c6
Bedros <boyajian@uchicago.edu>
2019-09-27 15:57:00 -0500

    adding bonus to nim

--- 20190930/U1M2L8PBD/src/main/java/com/company/Nim.java
+++ 20190930/U1M2L8PBD/src/main/java/com/company/Nim.java
@@ -3,8 +3,66 @@ package com.company;
 import java.util.Scanner;
 
 public class Nim {
+    Scanner scanner=new Scanner(System.in);
+
+    public String getPile(String activePlayer,int a,int b,int c){
+       String str;
+       int pile=0;
+        System.out.print(activePlayer+", choose a pile: ");
+        str=scanner.nextLine().toUpperCase();
+        switch (str){
+            case "A":
+                pile=a;
+                break;
+            case "B":
+                pile=b;
+                break;
+            case "C":
+                pile=c;
+                break;
+            default:
+                System.out.println("You did not enter a valid pile.");
+                str=getPile(activePlayer,a,b,c);
+        }
+        if (pile<=0){
+            System.out.println("Nice try, that pile is empty.");
+            str=getPile(activePlayer,a,b,c);
+        }
+        return str;
+
+    }
+
+    public int getAmnt(String str, int a,int b,int c){
+
+        System.out.print("How many to remove from Pile "+str.toUpperCase()+": ");
+        int amnt=Integer.parseInt(scanner.nextLine());
+        if (amnt<=0){
+            System.out.println("You must take at least 1 piece.");
+            amnt=getAmnt(str,a,b,c);
+        }
+        switch (str.toUpperCase()){
+            case "A":
+                if (a<amnt){
+                    System.out.println("Pile A does not have that many!");
+                    amnt=getAmnt(str,a,b,c);
+                };
+                break;
+            case "B":
+                if (b<amnt){
+                    System.out.println("Pile B does not have that many!");
+                    amnt=getAmnt(str,a,b,c);}
+                break;
+            case "C":
+                if (c<amnt){
+                    System.out.println("Pile C does not have that many!");
+                    amnt=getAmnt(str,a,b,c);}
+                break;
+        }
+        return amnt;
+    }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+        Nim nim=new Nim();
         int a=3,b=4,c=5;
         String str="";
         int amnt=0;
@@ -13,37 +71,36 @@ public class Nim {
         System.out.print("Player 2, enter your name: ");
         String player2=scanner.nextLine();
         String activePlayer="";
-        boolean isEven=false;
-        while(a>0||b>0||c>0){
+        boolean isEven=false,lost=false;
+        while(a+b+c>1){
+
             isEven=!isEven;
             if (isEven){activePlayer=player1;}
             else{activePlayer=player2;}
+
             System.out.printf("A: %d   B:  %d   C:  %d  \n",a,b,c);
-            System.out.print(activePlayer+", choose a pile: ");
-            str=scanner.nextLine();
             System.out.println();
-            System.out.print("How many to remove from Pile "+str.toUpperCase()+": ");
-            amnt=Integer.parseInt(scanner.nextLine());
+
+            str=nim.getPile(activePlayer,a,b,c);
+            amnt=nim.getAmnt(str,a,b,c);
             switch (str){
                 case "A":
-                case "a":
                     a-=amnt;
                     break;
                 case "B":
-                case"b":
                     b-=amnt;
                     break;
                 case "C":
-                case "c":
                     c-=amnt;
                     break;
             }
+            if (a+b+c<1){lost=true;}
+
         }
-        if (activePlayer.equals((player1))){activePlayer=player2;}
-        else{activePlayer=player1;}
+
         System.out.printf("A: %d   B:  %d   C:  %d  \n",a,b,c);
-        System.out.println("There are no tiles left, so "+activePlayer+" is the winner!");
-        System.out.println();
-        System.out.println("All piles are empty. Good job!");
+        if(!lost){
+        System.out.println("There is only one tile left, so "+activePlayer+" is the winner!");}else{
+            System.out.println(activePlayer+" could have won, but took all of the remaining tiles and lost.");}
     }
 }
