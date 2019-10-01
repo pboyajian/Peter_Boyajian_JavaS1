3044efb81857a29a07b9633a4af20abe3b2d1a74
Bedros <boyajian@uchicago.edu>
2019-10-01 08:55:13 -0500

    adding doubles to tests

--- 2019101/U1M3L1Katas/src/test/java/com/company/TestPhysics.java
+++ 2019101/U1M3L1Katas/src/test/java/com/company/TestPhysics.java
@@ -14,35 +14,35 @@ public class TestPhysics {
     }
     @Test
     public void shouldCalculatePositiveForce(){
-        assertEquals(12, physics.calculateForce(3, 4),delta);
+        assertEquals(12, physics.calculateForce(3.0, 4),delta);
     }
     @Test
     public void shouldCalculateNegativeForce(){
-        assertEquals(-12, physics.calculateForce(3, -4),delta);
+        assertEquals(-12, physics.calculateForce(3, -4.0),delta);
     }
     @Test
     public void shouldCalculateZeroForce(){
-        assertEquals(0, physics.calculateForce(3, 0),delta);
+        assertEquals(0, physics.calculateForce(3.0, 0),delta);
     }
     @Test
     public void shouldCalculatePositiveVelocity(){
-        assertEquals(3, physics.calculateVelocity(3,6,1,2),delta);
+        assertEquals(3, physics.calculateVelocity(3,6.0,1,2),delta);
     }
     @Test
     public void shouldCalculateNegativeVelocity(){
-        assertEquals(-3, physics.calculateVelocity(6,3,1,2),delta);
+        assertEquals(-3, physics.calculateVelocity(6,3.0,1,2),delta);
     }
     @Test
     public void shouldCalculateZeroVelocity(){
-        assertEquals(0, physics.calculateVelocity(6,6,1,2),delta);
+        assertEquals(0, physics.calculateVelocity(6.0,6,1,2),delta);
     }
     @Test
     public void shouldCalculatePositiveMass(){
-        assertEquals(10, physics.calculateMass(100, 10),delta);
+        assertEquals(10, physics.calculateMass(100, 10.0),delta);
     }
     @Test
     public void shouldCalculateZeroMass(){
-        assertEquals(0, physics.calculateMass(0, 10),delta);
+        assertEquals(0, physics.calculateMass(0.0, 10),delta);
     }
     @Test
     public void shouldReturnInfiniteMassInZeroGravity(){
@@ -58,25 +58,25 @@ public class TestPhysics {
     }
     @Test
     public void shouldCalculateZeroAcceleration(){
-        assertEquals(0, physics.calculateAcceleration(2,2,1,2),delta);
+        assertEquals(0, physics.calculateAcceleration(2.0,2,1,2),delta);
     }
     @Test
     public void shouldReturnInfiniteAccelerationWhenDividingByZero(){
-        assertEquals(Double.POSITIVE_INFINITY, physics.calculateAcceleration(1,2,1,1),delta);
+        assertEquals(Double.POSITIVE_INFINITY, physics.calculateAcceleration(1.0,2,1,1),delta);
         assertEquals(Double.NEGATIVE_INFINITY, physics.calculateAcceleration(2,1,1,1),delta);
     }
     @Test
     public void shouldReturnInfiniteVelocityWhenDividingByZero(){
-        assertEquals(Double.POSITIVE_INFINITY, physics.calculateVelocity(1,2,1,1),delta);
+        assertEquals(Double.POSITIVE_INFINITY, physics.calculateVelocity(1,2.0,1,1),delta);
         assertEquals(Double.NEGATIVE_INFINITY, physics.calculateVelocity(2,1,1,1),delta);
     }
     @Test
     public void shouldCalculatePositiveDistance(){
-        assertEquals(12, physics.calculateDistance(3, 4),delta);
+        assertEquals(12, physics.calculateDistance(3.0, 4),delta);
     }
     @Test
     public void shouldHandleZeroDistance(){
-        assertEquals(0, physics.calculateDistance(0, 4),delta);
+        assertEquals(0, physics.calculateDistance(0, 4.0),delta);
         assertEquals(0, physics.calculateDistance(4, 0),delta);
     }
 
