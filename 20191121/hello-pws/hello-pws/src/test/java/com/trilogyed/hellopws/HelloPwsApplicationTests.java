536e86f41b62e9619b8a346fd3f9ee3087c408c7
Bedros <boyajian@uchicago.edu>
2019-11-21 07:28:07 -0600

    tutorials for pws

---
+++ 20191121/hello-pws/hello-pws/src/test/java/com/trilogyed/hellopws/HelloPwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.hellopws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
